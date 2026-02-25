4bd277456a965ffb94a26fbe1ca7620cd0818f41
Thomas Benard <thomasagadabenard@gmail.com>
2026-02-25 10:09:20 +0100

    first commit

---
+++ DashBoard/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import NavBar from './components/NavBar'
+import Calender from './Pages/Calender'
+import Projects from './Pages/Projects'
+import Settings from './Pages/Settings'
+import Tasks from './Pages/Tasks'
+import Team from './Pages/Team'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+
+  return (
+    <>
+      
+      <BrowserRouter>
+        <div className='flex'>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/Calender' element = {<Calender />}/>
+            <Route path='/Projects' element = {<Projects />} />
+            <Route path = '/Settings' element = {<Settings />}/>
+            <Route path='/Tasks' element = {<Tasks />}/>
+            <Route path='/Team' element = {<Team />}/>
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+
+}
+
+export default App
---
+++ DashBoard/src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <aside className="side-bar">
+        <div className="task-flow">
+          <div className="logo-grid">
+            <span className="grid-one"></span>
+            <span></span>
+            <span className="grid-three"></span>
+            <span></span>
+          </div>
+          <div className="logo">TaskFlow</div>
+        </div>
+
+        <nav className="menu">
+          <Link to="/" className='menu-child active' > Dashboard </Link>
+          <Link to="/projects" className='menu-child'>Projects</Link>
+          <Link to="/tasks" className='menu-child'>Tasks</Link>
+          <Link to="/calendar" className='menu-child'>Calendar</Link>
+          <Link to="/team" className='menu-child'>Team</Link>
+          <Link to="/settings" className='menu-child'>Settings</Link>
+        </nav>
+
+        <div className="profile">
+          <img src="https://i.pravatar.cc/40" alt="user" />
+          <div>
+            <p className="name">Sarah Johnson</p>
+            <span>Product Manager</span>
+          </div>
+        </div>
+      </aside>
+    </>
+  )
+}
+
+export default NavBar
